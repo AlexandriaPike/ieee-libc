dc85e6b81fe80fac1a2b98617111225b29451136
alexandriapike <alexandriapike1@gmail.com>
2024-05-19 06:44:57 -0700

    begin removing cruft

--- src/time/asctime.c
+++
@@ -1,9 +0,0 @@
-#include <time.h>
-#include <stdio.h>
-
-char *asctime(const struct tm *timeptr)
-{
-	static char buf[40];
-	strftime(buf, sizeof(buf), "%a %b %e %H:%M:%S %Y\n", timeptr);
-	return buf;
-}
--- src/time/nanosleep.c
+++
@@ -1,8 +0,0 @@
-#include <sys/syscall.h>
-#include <time.h>
-
-int nanosleep(const struct timespec *rqtp, struct timespec *rmtp)
-{
-        return syscall(SYS_nanosleep, rqtp, rmtp);
-}
-
